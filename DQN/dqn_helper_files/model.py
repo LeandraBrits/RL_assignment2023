1c200e0b96178e2472413d7ea20bd01c0b503b35
Rebekahvw <101238947+Rebekahvw@users.noreply.github.com>
2023-10-29 18:19:55 +0200

    Adding comments

--- DQN/dqn_helper_files/model.py
+++ DQN/dqn_helper_files/model.py
@@ -1,31 +1,67 @@
+from gym import spaces
 import torch.nn as nn
-
+import torch
+import torch.nn.functional as F
+import gym
+from nle import nethack
+import minihack
+import numpy as np
 
 class DQN(nn.Module):
     """
-    A basic implementation of a Deep Q-Network. The architecture is the same as that described in the
-    Nature DQN paper.
+    A basic implementation of a Deep Q-Network.
     """
 
-    def __init__(self, observation_space, action_space):
+    def __init__(self, observation_space: spaces.Box, action_space: spaces.Discrete):
         """
         Initialise the DQN
         :param observation_space: the state space of the environment
         :param action_space: the action space of the environment
         """
+        
+        super().__init__() #initialises the parent class
 
-        # TODO Implement DQN Network
-        self.conv1 = nn.Conv2d(in_channels=observation_space.n, out_channels=32, kernel_size=8, stride=4)	
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
-        self.fc1 = nn.Linear(in_features=64 * 7 * 7, out_features=512)
-        self.fc2 = nn.Linear(in_features=512, out_features=action_space.n)
-        #raise NotImplementedError
+        assert (
+            type(observation_space) == spaces.Box
+        ), "observation_space must be of type Box"
+        assert (
+            len(observation_space.shape) == 3
+        ), "observation space must have the form channels x width x height"
+        assert (
+            type(action_space) == spaces.Discrete
+        ), "action_space must be of type Discrete"
+        
+        # convolutional layers
+        self.conv1 = nn.Conv2d(observation_space.shape[0], 32, 6, stride=3)
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, 2, stride=1)
+        
+        # get the dimensions of the input by doing a forward pass with fake data
+
+        input_size = self.get_input_size(observation_space.shape)
+
+        self.fc1 = nn.Linear(input_size, 512) #flattening
+        self.fc2 = nn.Linear(512, action_space.n) # 512 in, number of actions out, bc the number of actions is the number of classes.
+
+        # allows our code to be run on the GPU
+        self.device = torch.device("cuda")
+        self.to(self.device)
+
+    def get_input_size(self, input_dims):
+        # function that does a forward pass of the code so that we can find out the input size
+        state = torch.zeros(1, *input_dims)
+        dims = self.conv1(state)
+        dims = self.conv2(dims)
+        dims = self.conv3(dims)
+        return int(np.prod(dims.size()))
 
     def forward(self, x):
-        # TODO Implement forward pass
-        x = nn.functional.relu(self.conv1(x))
-        x = nn.functional.relu(self.conv2(x))
-        x = nn.functional.relu(self.conv3(x))
-        x = nn.functional.relu(self.fc1(x.view(x.size(0), -1)))
-        return self.fc2(x.view(x.size(0), -1))
+        # implements a forward pass through neural network
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.view(x.size()[0], -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x) # this is our output layer, so we dont need to use an activation function
+
+        return x
